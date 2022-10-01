6669a8bfd757fffb61d8aa0b52d30619e84c9755
Tanita <tanita.pak@gmail.com>
2022-10-01 14:31:36 +0600

    создана страница вывода записей гостей

--- app/webapp/views.py
+++ app/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from webapp.models import GuestBook
+
+
+def index_view(request):
+    guest_books = GuestBook.objects.order_by('create_at').filter(status="active")
+    context = {
+        'guest_books': guest_books
+    }
+    return render(request, 'index.html', context)
